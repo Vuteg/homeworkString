97728d679f0ba9b804a1a87769797d93b29a8958
vuteg <vandal-xp91@bk.ru>
2022-12-10 23:14:02 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Задание 1
+        System.out.println("Задание 1");
+        String firstName = "Семён";
+        String lastName = "Иванов";
+        String middleName = "Семёнович";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника -  " + fullName);
 
+        // Задание 2
+        System.out.println("Задание 2");
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName1);
 
+        // Задание 3
+        System.out.println("Задание 3");
+        fullName = fullName.replace('ё', 'е');
+        System.out.println(fullName);
     }
 }
